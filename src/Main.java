5a0b5291c0952f10d6cd039dc1c5f1413ffb4b59
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2023-01-30 02:11:21 -0500

    Fix bin tree level order trav

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
-import tlecla.leetcode.problems.MergeTwoSortedLists;
-import tlecla.leetcode.problems.NAryTreePreorderTraversal;
-import tlecla.leetcode.problems.Node;
+import tlecla.leetcode.problems.*;
 
 public class Main {
   public static void main(String[] args) {
-    NAryTreePreorderTraversal pre = new NAryTreePreorderTraversal();
-    Node node = new Node(3);
-    System.out.println(pre.preorder(node));
+    BinaryTreeLevelOrderTraversal bin = new BinaryTreeLevelOrderTraversal();
+    TreeNode tree = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+    System.out.println(bin.levelOrder(tree));
   }
 }
--- src/tlecla/leetcode/problems/BinaryTreeLevelOrderTraversal.java
+++ src/tlecla/leetcode/problems/BinaryTreeLevelOrderTraversal.java
@@ -2,24 +2,63 @@ package tlecla.leetcode.problems;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
+import java.util.Queue;
 
+
+/**
+ * 102. Binary Tree Level Order Traversal
+ * <p>
+ * <a href="https://leetcode.com/problems/binary-tree-level-order-traversal/?envType=study-plan&id=level-1">LeetCode link</a>
+ */
 public class BinaryTreeLevelOrderTraversal {
+  /*
+  Idea: BFS traversal and accumulate all node for level
+   */
   public List<List<Integer>> levelOrder(TreeNode root) {
+    if (root == null) return new LinkedList<>();
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
     List<List<Integer>> results = new LinkedList<>();
-    Stack<TreeNode> stack = new Stack<>();
-    stack.push(root);
-    int level = 0;
-    int iteration = 0;
-    while (!stack.isEmpty()) {
-      TreeNode currentParent = stack.pop();
-      if (currentParent.left != null) {
-        stack.add(currentParent.left);
+    while (!queue.isEmpty()) {
+      List<Integer> levelResults = new LinkedList<>();
+      int size = queue.size();
+      for (int i = 0; i < size; ++i) {
+        TreeNode current = queue.poll();
+        levelResults.add(current.val);
+        if (current.left != null) {
+          queue.add(current.left);
+        }
+        if (current.right != null) {
+          queue.add(current.right);
+        }
       }
-      if (currentParent.right != null) {
-        stack.add(currentParent.right);
-      }
+      results.add(levelResults);
+    }
+    return results;
+  }
 
+  public List<List<Integer>> levelOrder1(TreeNode root) {
+    if (root == null) return new LinkedList<>();
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+    List<List<Integer>> results = new LinkedList<>();
+    while (!queue.isEmpty()) {
+      List<Integer> levelResults = new LinkedList<>();
+      Queue<TreeNode> prevNodes = new LinkedList<>();
+      queue.forEach(treeNode -> {
+        levelResults.add(treeNode.val);
+        prevNodes.add(treeNode);
+      });
+      queue.clear();
+      results.add(levelResults);
+      prevNodes.forEach(treeNode -> {
+        if (treeNode.left != null) {
+          queue.add(treeNode.left);
+        }
+        if (treeNode.right != null) {
+          queue.add(treeNode.right);
+        }
+      });
     }
     return results;
   }
