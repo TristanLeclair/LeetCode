bc226bde1022e55456dedc1765b67c3987b71be2
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2023-02-01 01:23:34 -0500

    [BROKEN] start floodfill

---
+++ src/tlecla/leetcode/problems/FloodFill.java
@@ -0,0 +1,16 @@
+package tlecla.leetcode.problems;
+
+import java.util.Stack;
+
+/**
+ * 733. Flood Fill
+ * <p>
+ * <a href="https://leetcode.com/problems/flood-fill/">LeetCode link</a>
+ */
+public class FloodFill {
+  private boolean[][] visited;
+  public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+    visited = new boolean[image[0].length][image.length];
+    return null;
+  }
+}
