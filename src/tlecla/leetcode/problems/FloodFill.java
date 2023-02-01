fb369c02195161a653e142507f24ae882ea8f124
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2023-02-01 01:59:52 -0500

    Add FloodFill

--- src/tlecla/leetcode/problems/FloodFill.java
+++ src/tlecla/leetcode/problems/FloodFill.java
@@ -1,7 +1,5 @@
 package tlecla.leetcode.problems;
 
-import java.util.Stack;
-
 /**
  * 733. Flood Fill
  * <p>
@@ -9,8 +7,33 @@ import java.util.Stack;
  */
 public class FloodFill {
   private boolean[][] visited;
+  private int color;
+  private int initColor;
+
   public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-    visited = new boolean[image[0].length][image.length];
-    return null;
+    visited = new boolean[image.length][image[0].length];
+    this.color = color;
+    this.initColor = image[sr][sc];
+    floodFillRecursive(image, sr, sc);
+    return image;
+  }
+
+  private void floodFillRecursive(int[][] image, int sr, int sc) {
+    if (isValid(image, sr, sc)) {
+      visited[sr][sc] = true;
+      image[sr][sc] = color;
+      visitNeighbours(image, sr, sc);
+    }
+  }
+
+  private void visitNeighbours(int[][] image, int sr, int sc) {
+    floodFillRecursive(image, sr - 1, sc);
+    floodFillRecursive(image, sr + 1, sc);
+    floodFillRecursive(image, sr, sc + 1);
+    floodFillRecursive(image, sr, sc - 1);
+  }
+
+  private boolean isValid(int[][] image, int sr, int sc) {
+    return !(sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || visited[sr][sc] || image[sr][sc] != initColor);
   }
 }
