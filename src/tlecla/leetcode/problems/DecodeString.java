4a8043fc5782af22b7412c31c30c88f2b5811fac
Tristan Leclair-Vani <60434271+TristanLeclair@users.noreply.github.com>
2024-01-09 11:14:44 -0500

    Add DecodeString and TwoSum

---
+++ src/tlecla/leetcode/problems/DecodeString.java
@@ -0,0 +1,18 @@
+package tlecla.leetcode.problems;
+
+import java.util.Stack;
+
+/**
+ * 394. Decode String
+ * <p>
+ * <a href="https://leetcode.com/problems/decode-string/">LeetCode link</a>
+ */
+public class DecodeString {
+  public String decodeString(String s) {
+    Stack<Character> stack = new Stack<>();
+    stack.push(s.charAt(0));
+    while(!stack.isEmpty()) {
+      char curr = stack.pop();
+    }
+  }
+}
